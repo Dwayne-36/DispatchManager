4811eeae98bfe9fe7c7ffe8e7006ded1c5a16407
Dwayne Keast <dwaynekeast@hotmail.com>
2025-06-27 13:49:19 +1200

    controls added

--- DispatchManager/Forms/FrmViewDispatch.Designer.cs
+++ DispatchManager/Forms/FrmViewDispatch.Designer.cs
@@ -28,36 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dgvDispatch = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvDispatch)).BeginInit();
+            this.dgvSchedule = new System.Windows.Forms.DataGridView();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnNewProject = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSchedule)).BeginInit();
             this.SuspendLayout();
             // 
-            // dgvDispatch
+            // dgvSchedule
             // 
-            this.dgvDispatch.AllowUserToAddRows = false;
-            this.dgvDispatch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvDispatch.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvDispatch.Location = new System.Drawing.Point(0, 0);
-            this.dgvDispatch.Name = "dgvDispatch";
-            this.dgvDispatch.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvDispatch.Size = new System.Drawing.Size(800, 450);
-            this.dgvDispatch.TabIndex = 0;
+            this.dgvSchedule.AllowUserToAddRows = false;
+            this.dgvSchedule.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvSchedule.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSchedule.Location = new System.Drawing.Point(1, 126);
+            this.dgvSchedule.Name = "dgvSchedule";
+            this.dgvSchedule.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvSchedule.Size = new System.Drawing.Size(1190, 649);
+            this.dgvSchedule.TabIndex = 0;
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.Location = new System.Drawing.Point(97, 15);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(200, 20);
+            this.dtpFrom.TabIndex = 1;
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(16, 19);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(66, 13);
+            this.lblFrom.TabIndex = 2;
+            this.lblFrom.Text = "Select From:";
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.Location = new System.Drawing.Point(97, 42);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(200, 20);
+            this.dtpTo.TabIndex = 3;
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(26, 46);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(56, 13);
+            this.lblTo.TabIndex = 4;
+            this.lblTo.Text = "Select To:";
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(97, 78);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(200, 20);
+            this.tbSearch.TabIndex = 5;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(38, 83);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(44, 13);
+            this.lblSearch.TabIndex = 6;
+            this.lblSearch.Text = "Search:";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(304, 77);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnNewProject
+            // 
+            this.btnNewProject.Location = new System.Drawing.Point(499, 15);
+            this.btnNewProject.Name = "btnNewProject";
+            this.btnNewProject.Size = new System.Drawing.Size(70, 59);
+            this.btnNewProject.TabIndex = 8;
+            this.btnNewProject.Text = "New Project";
+            this.btnNewProject.UseVisualStyleBackColor = true;
             // 
             // FrmViewDispatch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dgvDispatch);
+            this.ClientSize = new System.Drawing.Size(1191, 775);
+            this.Controls.Add(this.btnNewProject);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.dtpTo);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.dtpFrom);
+            this.Controls.Add(this.dgvSchedule);
             this.Name = "FrmViewDispatch";
-            this.Text = "FrmViewDispatch";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvDispatch)).EndInit();
+            this.Text = "Haylo View";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSchedule)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dgvDispatch;
+        private System.Windows.Forms.DataGridView dgvSchedule;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnNewProject;
     }
 }
